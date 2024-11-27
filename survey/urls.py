a29fdf794ccda0ca52c97bc6a44641bcb0fcbe59
Ruslan Davletov <davrr555@gmail.com>
2024-11-27 04:22:11 +0300

    add

--- survey/urls.py
+++ survey/urls.py
@@ -5,4 +5,5 @@ from survey import views
 app_name = 'survey'
 urlpatterns = [
     path('', views.survey_index, name='survey_index'),
+    path('second', views.survey_second, name='survey_second'),
 ]
