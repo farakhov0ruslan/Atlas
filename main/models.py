66e91d44e22e2531daf4c33e9a286a6b5068b374
Ruslan Davletov <davrr555@gmail.com>
2024-12-05 17:49:32 +0300

    bd

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Travel(models.Model):
+    destination = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    traveler = models.CharField(max_length=50)
+    notes = models.TextField()
