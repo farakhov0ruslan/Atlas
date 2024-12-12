7a3dbd23b8698e9f3e4c2f6e34e28c14ee731597
Ruslan Farakhov <Ruslan Farakhov>
2024-12-12 16:34:57 +0300

    develop: изменил главную страницу, добавил поодержку медиафайлов и шаблонизатор

--- Atlas/urls.py
+++ Atlas/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from Atlas import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace="main")),
     path('survey/', include('survey.urls', namespace='survey'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include("debug_toolbar.urls"))
+    ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
