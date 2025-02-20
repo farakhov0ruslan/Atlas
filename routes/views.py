8dd3a5b753fbf2b2bc3b9f682e1a62b8e5e079cd
Ruslan Farakhov <Ruslan Farakhov>
2025-02-21 02:15:31 +0300

    develop: проброс данных

--- routes/views.py
+++ routes/views.py
@@ -12,12 +12,21 @@ def route_page(request):
     return_date = request.session.get('return_date', 'Не указано')
     person_count = request.session.get('person_count', 'Не указано')
     budget = request.session.get('budget', 'Не указано')
+    selected_categories = request.session.get('selected_categories', [])
+    selected_images = request.session.get('selected_images', [])
+    # Получаем предпочтения из сессии по ключу 'preferences'
+    if request.method == "POST":
+        preferences = request.POST.get('preferences', '')
+        request.session['preferences'] = preferences
+
+    print(request.session.items())
 
     # Вычисляем количество дней
     try:
         departure_date = datetime.strptime(departure_date, '%Y-%m-%d')
         return_date = datetime.strptime(return_date, '%Y-%m-%d')
         days_count = (return_date - departure_date).days + 1  # Количество дней
+        print(days_count)
         days = [f"День {i + 1}" for i in
                 range(days_count)]  # Формируем список "День 1", "День 2" и т.д.
     except ValueError:
@@ -45,6 +54,7 @@ def route_page(request):
         route = None
 
     if not route:
+        print()
         user_preferences = f"""
             Я хочу завтрак каждый день. Поездка на {days_count} дней
             """
@@ -58,8 +68,7 @@ def route_page(request):
                 activity["place"] = Place.objects.get(id=activity["place_id"])
             else:
                 activity["place"] = None
-    print(request.session)
-    print(route)
+
     context = {
         "title": "Ваш маршрут",
         "city": city,
