d2bc04ffff10ef29282e3f66e6f17672a38b3806
Ruslan Davletov <davrr555@gmail.com>
2025-02-26 23:35:46 +0300

    update

--- main/views.py
+++ main/views.py
@@ -6,25 +6,26 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import JsonResponse
 
-# def survey_redirect(request):
-#     if request.method == "POST":
-#         # Получаем данные формы
-#         city = request.POST.get("city")
-#         person_count = request.POST.get("person_count")
-#         budget = request.POST.get("budget", "Не указано")
-#         departure_date = request.POST.get("departure_date")
-#         return_date = request.POST.get("return_date")
-#
-#         # Сохраняем данные в сессии для перенаправления
-#         request.session['city'] = city
-#         request.session['person_count'] = person_count
-#         request.session['budget'] = budget
-#         request.session['departure_date'] = departure_date
-#         request.session['return_date'] = return_date
-#
-#         # Перенаправляем на страницу опроса
-#         return redirect("survey:survey_index")
-#     return redirect("your_template_url")
+def survey_redirect(request):
+    if request.method == "POST":
+        # Получаем данные формы
+        city = request.POST.get("city")
+        person_count = request.POST.get("person_count")
+        budget = request.POST.get("budget", "Не указано")
+        departure_date = request.POST.get("departure_date")
+        return_date = request.POST.get("return_date")
+
+        # Сохраняем данные в сессии для перенаправления
+        request.session['city'] = city
+        request.session['person_count'] = person_count
+        request.session['budget'] = budget
+        request.session['departure_date'] = departure_date
+        request.session['return_date'] = return_date
+
+        # Перенаправляем на страницу опроса
+        return redirect("survey:survey_index")
+
+    return redirect("your_template_url")
 
 
 def index(request):
@@ -71,7 +72,7 @@ def login_view(request):
             return JsonResponse({"success": True, "redirect_url": '/'})  # Возвращаем JSON для успешного входа
         else:
             # Если аутентификация не прошла
-            return JsonResponse({"success": False, "errors": {"email": "Неверные данные для входа.", "password": "Неверные данные для входа."}})
+            return JsonResponse({"success": False, "errors": {"password": "Неверные данные для входа."}})
 
     return render(request, 'main/index.html')
 
